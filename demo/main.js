43250c86007500d42b639dffb58e238000125198
vfasky <vfasky@gmail.com>
2014-09-22 16:58:18 +0800

    fix

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,8 @@
 ﻿require.config({
     baseUrl:'../',
+    shim:{
+        'foundation-datepicker': ['jquery']
+    },
     paths: {
         'jquery': './demo/vendor/jquery/jquery-1.11.0.min',
         'foundation-datepicker': './demo/vendor/foundation-datepicker/foundation-datepicker',
