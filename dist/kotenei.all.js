b3f2d0807efa5c3677b783ab6aeb94b76e86fa0b
kotenei <kotenei@sina.com>
2014-09-24 17:55:37 +0800

    增加无限滚动

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -769,6 +769,58 @@ function($, Dropdown, util){
 
     return Datepicker;
 });
+/**
+ * 无限滚动模块
+ * @date :2014-09-24
+ * @author kotenei (kotenei@qq.com)
+ */
+define('kotenei/infiniteScroll', ['jquery'], function ($) {
+
+    /**
+     * 无限滚动模块
+     * @param {Object} options - 参数
+     */
+    var InfiniteScroll = function (options) {
+        var self = this;
+        this.options = $.extend({}, {
+            $scrollElement: $(window),
+            $watchElement: null,
+            scrollDistance: 0,
+            callback: $.noop
+        }, options);
+        this.$scrollElement = this.options.$scrollElement;
+        this.$watchElement = this.options.$watchElement;
+
+        if (!this.$watchElement) { return; }
+ 
+        this.$scrollElement.on('scroll.infiniteScroll', function () {
+            self.scroll();
+        });
+
+        this.scroll();       
+    };
+
+    /**
+     * 滚动操作
+     * @return {Void}       
+     */
+    InfiniteScroll.prototype.scroll = function () {
+        var scrollElmHeight = this.$scrollElement.height();
+        var scrollBottom = scrollElmHeight + this.$scrollElement.scrollTop();
+        var watchElmBottom = this.$watchElement.offset().top + this.$watchElement.height();
+        var remaining = watchElmBottom - scrollBottom;
+        var canScroll = remaining <= scrollElmHeight * this.options.scrollDistance;
+        if (canScroll) {
+            if (this.options.callback() === false) {
+                this.$scrollElement.off('scroll.infiniteScroll');
+            }
+        }
+    };
+
+
+    return InfiniteScroll;
+});
+
 /*
  * 图片延迟加载模块
  * @date:2014-09-01
@@ -2938,12 +2990,13 @@ define('kotenei/wordLimit', ['jquery'], function ($) {
     return WordLimit;
 });
 ;
-define("kotenei", ["kotenei/autoComplete", "kotenei/dragdrop", "kotenei/dropdown", "kotenei/dropdownDatepicker", "kotenei/lazyload", "kotenei/loading", "kotenei/pager", "kotenei/placeholder", "kotenei/popTips", "kotenei/router", "kotenei/slider", "kotenei/switch", "kotenei/tooltips", "kotenei/util", "kotenei/validate", "kotenei/validateTooltips", "kotenei/window", "kotenei/wordLimit"], function(_autoComplete, _dragdrop, _dropdown, _dropdownDatepicker, _lazyload, _loading, _pager, _placeholder, _popTips, _router, _slider, _switch, _tooltips, _util, _validate, _validateTooltips, _window, _wordLimit){
+define("kotenei", ["kotenei/autoComplete", "kotenei/dragdrop", "kotenei/dropdown", "kotenei/dropdownDatepicker", "kotenei/infiniteScroll", "kotenei/lazyload", "kotenei/loading", "kotenei/pager", "kotenei/placeholder", "kotenei/popTips", "kotenei/router", "kotenei/slider", "kotenei/switch", "kotenei/tooltips", "kotenei/util", "kotenei/validate", "kotenei/validateTooltips", "kotenei/window", "kotenei/wordLimit"], function(_autoComplete, _dragdrop, _dropdown, _dropdownDatepicker, _infiniteScroll, _lazyload, _loading, _pager, _placeholder, _popTips, _router, _slider, _switch, _tooltips, _util, _validate, _validateTooltips, _window, _wordLimit){
     return {
         "AutoComplete" : _autoComplete,
         "Dragdrop" : _dragdrop,
         "Dropdown" : _dropdown,
         "DropdownDatepicker" : _dropdownDatepicker,
+        "InfiniteScroll" : _infiniteScroll,
         "lazyload" : _lazyload,
         "Loading" : _loading,
         "Pager" : _pager,
---
+++ js/kotenei/infiniteScroll.js
@@ -0,0 +1,51 @@
+/**
+ * 无限滚动模块
+ * @date :2014-09-24
+ * @author kotenei (kotenei@qq.com)
+ */
+define('kotenei/infiniteScroll', ['jquery'], function ($) {
+
+    /**
+     * 无限滚动模块
+     * @param {Object} options - 参数
+     */
+    var InfiniteScroll = function (options) {
+        var self = this;
+        this.options = $.extend({}, {
+            $scrollElement: $(window),
+            $watchElement: null,
+            scrollDistance: 0,
+            callback: $.noop
+        }, options);
+        this.$scrollElement = this.options.$scrollElement;
+        this.$watchElement = this.options.$watchElement;
+
+        if (!this.$watchElement) { return; }
+ 
+        this.$scrollElement.on('scroll.infiniteScroll', function () {
+            self.scroll();
+        });
+
+        this.scroll();       
+    };
+
+    /**
+     * 滚动操作
+     * @return {Void}       
+     */
+    InfiniteScroll.prototype.scroll = function () {
+        var scrollElmHeight = this.$scrollElement.height();
+        var scrollBottom = scrollElmHeight + this.$scrollElement.scrollTop();
+        var watchElmBottom = this.$watchElement.offset().top + this.$watchElement.height();
+        var remaining = watchElmBottom - scrollBottom;
+        var canScroll = remaining <= scrollElmHeight * this.options.scrollDistance;
+        if (canScroll) {
+            if (this.options.callback() === false) {
+                this.$scrollElement.off('scroll.infiniteScroll');
+            }
+        }
+    };
+
+
+    return InfiniteScroll;
+});
