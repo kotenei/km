1daea69b86cfcb544c669213b5baf73454a37183
kotenei <kotenei@sina.com>
2014-09-24 12:42:38 +0800

    更新自动完成

--- js/kotenei/autoComplete.js
+++ js/kotenei/autoComplete.js
@@ -5,20 +5,24 @@
  */
 define('kotenei/autoComplete', ['jquery'], function ($) {
 
-
+    /**
+     * keycode
+     * @type {Object}
+     */
     var KEY = {
+        LEFT:37,
         UP: 38,
+        RIGHT:39,
         DOWN: 40,
-        DEL: 46,
         TAB: 9,
-        ENTER: 13,
-        ESC: 27,
-        COMMA: 188,
-        PAGEUP: 33,
-        PAGEDOWN: 34,
-        BACKSPACE: 8
+        ENTER: 13    
     };
 
+    /**
+     * 自动完成模块
+     * @param {JQuery} $element - dom
+     * @param {Object} options - 参数
+     */
     var AutoComplete = function ($element, options) {
         this.$element = $element;
         this.options = $.extend({}, {
@@ -28,35 +32,54 @@ define('kotenei/autoComplete', ['jquery'], function ($) {
             max: 10,
             formatItem: function (item) { return item; }
         }, options);
-        this.tpl = '<div class="k-autocomplete"></div>'
+        this.tpl = '<div class="k-autocomplete"></div>';
+        this.active = 0;
         this.init();
-    }
+    };
 
+    /**
+     * 初始化
+     * @return {Void}
+     */
     AutoComplete.prototype.init = function () {
         var self = this;
-        this.$list = $(this.tpl).hide().appendTo(document.body);
+        this.$listBox = $(this.tpl).hide().appendTo(document.body);
         this.data = this.options.data || [];
         this.$element.on('keyup', function (e) {
             var $this = $(this),
                 val = $.trim($this.val());
-            self.search(val);
+
+            if (!self.cache) {
+                self.cache = val;
+                self.search(val);
+                self.active = 0;
+            } else if (self.cache != val) {
+                self.cache = val;
+                self.search(val);
+                self.active = 0;
+            }
+
 
             switch (e.keyCode) {
                 case KEY.UP:
+                case KEY.LEFT:
                     e.preventDefault();
+                    self.prev();
                     break;
                 case KEY.DOWN:
+                case KEY.RIGHT:
+                    self.next();
                     break;
                 case KEY.ENTER:
-                    break;
                 case KEY.TAB:
+                    self.select();
                     break;
                 default:
                     break;
             }
         });
 
-        this.$list.on('click', 'li', function () {
+        this.$listBox.on('click', 'li', function () {
             var text = $(this).text();
             self.$element.val(text);
         });
@@ -66,6 +89,11 @@ define('kotenei/autoComplete', ['jquery'], function ($) {
         });
     };
 
+    /**
+     * 搜索数据
+     * @param  {String} value - 输入值
+     * @return {Void}       
+     */
     AutoComplete.prototype.search = function (value) {
 
         if (this.options.remote) {
@@ -77,44 +105,131 @@ define('kotenei/autoComplete', ['jquery'], function ($) {
         }
     };
 
+    /**
+     * 获取数据
+     * @param  {String} value - 输入值
+     * @return {Array}     
+     */
     AutoComplete.prototype.getData = function (value) {
-        var data = [];
+        var data = [], flag = 0;
         if (value.length === 0) { return data; }
         for (var i = 0, formatted; i < this.data.length; i++) {
             formatted = this.options.formatItem(this.options.data[i]);
-            if (formatted.toLowerCase().indexOf(value) == 0) {
+            if (formatted.toLowerCase().indexOf(value.toLowerCase()) == 0) {
                 data.push(formatted);
+                if (flag === (this.options.max - 1)) {
+                    break;
+                }
+                flag++;
             }
         }
         return data;
     };
 
-
+    /**
+     * 构造列表
+     * @param  {Array} data - 数据
+     * @return {Void}    
+     */
     AutoComplete.prototype.build = function (data) {
-        this.$list.find('ul').remove();
+        this.$listBox.find('ul').remove();
+        this.$listItem = null;
         if (data.length === 0) { return; }
         var html = '<ul>';
         for (var i = 0; i < data.length; i++) {
-            html += '<li>' + data[i] + '</li>';
-            if (i === this.options.max - 1) {
-                break;
-            }
+            html += '<li class="' + (i == 0 ? "active" : "") + '">' + data[i] + '</li>';
         }
         html += '</ul>';
-        this.$list.append(html);
+        this.$listBox.append(html);
+        this.$list = this.$listBox.find('ul');
+        this.$listItem = this.$listBox.find('li');
     };
 
+    /**
+     * 显示列表
+     * @return {Void}
+     */
     AutoComplete.prototype.show = function () {
-        if (this.$list.find("ul").length === 0) { this.hide(); return; }
-        this.$list.show().css({
+        if (!this.hasItem()) { this.hide(); return; }
+        this.$listBox.show().css({
             left: this.$element.position().left,
             top: this.$element.outerHeight() + this.$element.position().top,
             width: this.$element.outerWidth()
         });
     };
 
+    /**
+     * 隐藏列表
+     * @return {Void} 
+     */
     AutoComplete.prototype.hide = function () {
-        this.$list.hide();
+        this.$listBox.hide();
+    };
+
+    /**
+     * 移动到上一项
+     * @return {Void} 
+     */
+    AutoComplete.prototype.prev = function () {
+        this.moveSelect(-1);
+    };
+
+    /**
+     * 移动下一项
+     * @return {Void}
+     */
+    AutoComplete.prototype.next = function () {
+        this.moveSelect(1);
+    };
+
+    /**
+     * 判断是否有列表项
+     * @return {Boolean} 
+     */
+    AutoComplete.prototype.hasItem = function () {
+        return this.$listItem && this.$listItem.length > 0;
+    };
+
+    /**
+     * 移动到选择项
+     * @param  {Number} step - 移动步数
+     * @return {Void}    
+     */
+    AutoComplete.prototype.moveSelect = function (step) {
+        if (!this.hasItem()) { return; }
+        this.active += step;
+        if (this.active < 0) {
+            this.active = this.$listItem.length - 1;
+        } else if (this.active > this.$listItem.length - 1) {
+            this.active = 0;
+        }
+        var $curItem = this.$listItem.removeClass('active').eq(this.active).addClass('active');
+        var offset = 0;
+        this.$listItem.each(function () {
+            offset += this.offsetHeight;
+        });
+
+        var listScrollTop = this.$list.scrollTop(),
+            clientHeight = this.$list[0].clientHeight,
+            itemHeight = $curItem.height(),
+            itemTop = $curItem.position().top;
+
+        if (itemTop > clientHeight) {
+            this.$list.scrollTop(itemTop + itemHeight - clientHeight + listScrollTop);
+        } else if (itemTop < 0) {
+            this.$list.scrollTop(listScrollTop + itemTop)
+        }
+
+    };
+
+    /**
+     * 选择项
+     * @return {Void} 
+     */
+    AutoComplete.prototype.select = function () {
+        var $item = this.$listBox.find('li.active');
+        this.$element.val($item.text());
+        this.hide();
     };
 
     return AutoComplete;
