82395f3dc586c3d6f7ad6130859414dc5f920454
kotenei <kotenei@sina.com>
2014-10-20 14:31:40 +0800

    update

--- js/kotenei/clipZoom.js
+++ js/kotenei/clipZoom.js
@@ -5,7 +5,37 @@
  */
 define('kotenei/clipZoom', ['jquery', 'kotenei/dragdrop'], function ($,DragDrop) {
 
-    var ClipZoom = function () { };
+    var ClipZoom = function ($element, options) {
+        this.$element = $element;
+        this.options = $.extend({}, {
+            scale:true,    
+            selectorWidth: 120,
+            selectorHeight:100
+        }, options);
+    };
+
+    //初始化
+    ClipZoom.prototype.init = function () {
+
+    }
+
+    //裁剪
+    ClipZoom.prototype.clip = function () { };
+
+    //居中
+    ClipZoom.prototype.center = function () { };
+
+    //重置
+    ClipZoom.prototype.reset = function () { };
+
+    //缩放
+    ClipZoom.prototype.zoom = function () { };
+
+    //设置预览
+    ClipZoom.prototype.setPreview = function () { };
+
+    //图片加载
+    ClipZoom.prototype.imgLoad = function () { };
 
     return ClipZoom;
 
