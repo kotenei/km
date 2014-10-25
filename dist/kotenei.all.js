435ffa2991c3f6ca4558b69709750fb29433bbb5
kotenei <kotenei@sina.com>
2014-10-26 00:10:03 +0800

    update

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -2884,16 +2884,44 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
 
     var view = {
         getLineHtml: function (node) {
-            return '';
+            var lineType = _consts.line.CENTER;
+
+            if (node.isFirst && node.parentId === 0) {
+                lineType = _consts.line.ROOT;
+            } else if (node.isLast) {
+                lineType = _consts.line.BOTTOM;
+            }
+
+
+            if (node.hasChildren) {
+                if (node.open) {
+                    lineType += "_" + _consts.floder.OPEN;
+                } else {
+                    lineType += "_" + _consts.floder.CLOSE;
+                }
+            } else {
+                lineType += "_" + _consts.floder.DOCU;
+            }
+
+            return '<span class="' + _consts.className.ICON + ' ' + _consts.className.SWITCH + ' ' + lineType + '"></span>';
         },
         getIconHtml: function (node) {
-
+            html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.DOCU + '"></span>';
+            if (node.hasChildren) {
+                //有子节点
+                if (node.open) {
+                    html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.OPEN + '"></span>';
+                } else {
+                    html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.CLOSE + '"></span>';
+                }
+            }
+            return html;
         },
         getChkHtml: function (node, options) {
             if (!options.check.enable || node.noCheck) {
                 return '';
             }
-            return '<span class="' +_consts.className.ICON+ ' chk ' + options.check.chkType + '_' + String(node.checked === true) + '_' + (node.chkDisabled ? 'part' : 'full') + '"></span>';
+            return '<span class="' + _consts.className.ICON + ' chk ' + options.check.chkType + '_' + String(node.checked === true) + '_' + (node.chkDisabled ? 'part' : 'full') + '"></span>';
 
         },
         getOperateHtml: function (node, options) {
@@ -2947,16 +2975,35 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
                 node.isLast = true;
             }
 
+            node.hasChildren = this.hasChildren(node);
             this.nodes[this.prefix + node.nodeId] = node;
             this.initNodes(node.nodes);
         }
     };
 
     Tree.prototype.eventBind = function () {
+        var self = this;
 
-        this.$element.on('click', _consts.className.SWITCH, function () {
+        this.$element.on('click', "." + _consts.className.SWITCH, function () {
+            //展开
+            var $this = $(this),
+                $children = $this.parent().children('ul');
+            if ($children.length === 0) { return; }
+            $children.slideToggle('fast');
 
-        });
+        }).on('click', 'a', function () {
+            //选择
+            var $this = $(this);
+            if ($this.hasClass(_consts.node.SELECTED)) { return; }
+            self.$element.find('a').removeClass(_consts.node.SELECTED);
+            $this.addClass(_consts.node.SELECTED);
+        }).on('click', '.add', function () {
+            //添加
+        }).on('click', '.edit', function () {
+            //编辑
+        }).on('click', '.remove', function () {
+            //删除
+        })
 
     };
 
@@ -2967,13 +3014,19 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
         this.$element.append($elm);
     };
 
-    Tree.prototype.createNode = function (data, html) {
+    Tree.prototype.createNode = function (data, html, parent) {
+
+        var node;
 
         if (!utils.isArray(data) || data.length === 0 || !utils.isArray(html)) {
             return;
         }
 
-        html.push('<ul>');
+        if (parent && !parent.open) {
+            html.push('<ul style="display:none;" class="line">');
+        } else {
+            html.push('<ul class="line">');
+        }
 
         for (var i = 0, node; i < data.length; i++) {
             node = this.getNode(data[i].nodeId);
@@ -2986,7 +3039,7 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
                 html.push('<span>' + node.text + '</span>');
                 html.push(view.getOperateHtml(node, this.options));
                 html.push('</a>');
-                this.createNode(node.nodes, html);
+                this.createNode(node.nodes, html, node);
                 html.push('</li>');
             }
         }
@@ -2994,7 +3047,7 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
         html.push('</ul>');
     };
 
-    Tree.prototype.addNode = function (node) {
+    Tree.prototype.addNode = function (node, parent) {
         this.initNodes([node]);
     };
 
@@ -3006,9 +3059,11 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
         return this.nodes[this.prefix + id];
     };
 
-    Tree.prototype.hasNode = function (id) {
-        var node = this.getNode(id);
-        return node !== null && node !== undefined;
+    Tree.prototype.hasChildren = function (node) {
+        if (node && utils.isArray(node.nodes) && node.nodes.length > 0) {
+            return true;
+        }
+        return false;
     };
 
     return Tree;
