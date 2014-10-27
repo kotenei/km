cba90e9a5f63f02dce017af457159b12e9d2be1d
kotenei <kotenei@sina.com>
2014-10-27 15:29:20 +0800

    update

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -2903,16 +2903,16 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
                 lineType += "_" + _consts.floder.DOCU;
             }
 
-            return '<span class="' + _consts.className.ICON + ' ' + _consts.className.SWITCH + ' ' + lineType + '"></span>';
+            return '<span id="switch_' + node.nodeId + '" nId="' + node.nodeId + '" class="' + _consts.className.ICON + ' ' + _consts.className.SWITCH + ' ' + lineType + '"></span>';
         },
         getIconHtml: function (node) {
-            html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.DOCU + '"></span>';
+            html = '<span id="' + _consts.className.ICON + '_' + node.nodeId + '" class="' + _consts.className.ICON + ' ico_' + _consts.floder.DOCU + '"></span>';
             if (node.hasChildren) {
                 //有子节点
                 if (node.open) {
-                    html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.OPEN + '"></span>';
+                    html = '<span id="' + _consts.className.ICON + '_' + node.nodeId + '"  class="' + _consts.className.ICON + ' ico_' + _consts.floder.OPEN + '"></span>';
                 } else {
-                    html = '<span class="' + _consts.className.ICON + ' ico_' + _consts.floder.CLOSE + '"></span>';
+                    html = '<span id="' + _consts.className.ICON + '_' + node.nodeId + '"  class="' + _consts.className.ICON + ' ico_' + _consts.floder.CLOSE + '"></span>';
                 }
             }
             return html;
@@ -2921,7 +2921,7 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
             if (!options.check.enable || node.noCheck) {
                 return '';
             }
-            return '<span class="' + _consts.className.ICON + ' chk ' + options.check.chkType + '_' + String(node.checked === true) + '_' + (node.chkDisabled ? 'part' : 'full') + '"></span>';
+            return '<span id="chk_' + node.nodeId + '" nId="' + node.nodeId + '" class="' + _consts.className.ICON + ' chk ' + options.check.chkType + '_' + String(node.checked === true) + '_' + (node.chkDisabled ? 'part' : 'full') + '"></span>';
 
         },
         getOperateHtml: function (node, options) {
@@ -2932,16 +2932,25 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
             }
 
             if (options.edit.showAddBtn) {
-                str.push('<span class="' + _consts.className.ICON + ' add"></span>');
+                str.push('<span id="add_' + node.nodeId + '" nId="' + node.nodeId + '" class="' + _consts.className.ICON + ' add"></span>');
             }
             if (options.edit.showEditBtn) {
-                str.push('<span class="' + _consts.className.ICON + ' edit"></span>');
+                str.push('<span id="edit_' + node.nodeId + '" nId="' + node.nodeId + '" class="' + _consts.className.ICON + ' edit"></span>');
             }
             if (options.edit.showRemoveBtn) {
-                str.push('<span class="' + _consts.className.ICON + ' remove"></span>');
+                str.push('<span id="remove_' + node.nodeId + '" nId="' + node.nodeId + '"  class="' + _consts.className.ICON + ' remove"></span>');
             }
 
             return str.join('');
+        },
+        replaceClass: function (element) {
+            var className = element.className;
+            if (className.indexOf(_consts.floder.OPEN) !== -1) {
+                className = className.replace(new RegExp(_consts.floder.OPEN, 'ig'), _consts.floder.CLOSE);
+            } else {
+                className = className.replace(new RegExp(_consts.floder.CLOSE, 'ig'), _consts.floder.OPEN);
+            }
+            element.className = className;
         }
     };
 
@@ -2985,11 +2994,25 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
         var self = this;
 
         this.$element.on('click', "." + _consts.className.SWITCH, function () {
-            //展开
+            //展开/收缩
             var $this = $(this),
-                $children = $this.parent().children('ul');
+                id = $this.attr('nId'),
+                $children = self.$element.find('#ul_' + id),
+                $icon = self.$element.find('#icon_' + id);
+
             if ($children.length === 0) { return; }
             $children.slideToggle('fast');
+            view.replaceClass(this);
+            view.replaceClass($icon[0]);
+        }).on('click', '.chk', function () {
+            var $this = $(this),
+                id = $this.attr('nId'),
+                node = self.getNode(id);
+            if ($this.hasClass('checkbox_false_part')) { return; }
+
+            var nodes = self.getPCNodes(node);
+
+            console.log(nodes)
 
         }).on('click', 'a', function () {
             //选择
@@ -2999,12 +3022,14 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
             $this.addClass(_consts.node.SELECTED);
         }).on('click', '.add', function () {
             //添加
+            var $this = $(this);
         }).on('click', '.edit', function () {
             //编辑
+            var $this = $(this);
         }).on('click', '.remove', function () {
             //删除
-        })
-
+            var $this = $(this);
+        });
     };
 
     Tree.prototype.createTree = function () {
@@ -3016,25 +3041,35 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
 
     Tree.prototype.createNode = function (data, html, parent) {
 
-        var node;
+        var node, line = 'line';
 
         if (!utils.isArray(data) || data.length === 0 || !utils.isArray(html)) {
             return;
         }
 
-        if (parent && !parent.open) {
-            html.push('<ul style="display:none;" class="line">');
+        if (parent) {
+
+            if (parent.isLast) {
+                line = '';
+            }
+
+            if (!parent.open) {
+                html.push('<ul id="ul_' + parent.nodeId + '" style="display:none;" class="' + line + '" >');
+            } else {
+                html.push('<ul id="ul_' + parent.nodeId + '" class="' + line + '">');
+            }
+
         } else {
-            html.push('<ul class="line">');
+            html.push('<ul>');
         }
 
         for (var i = 0, node; i < data.length; i++) {
             node = this.getNode(data[i].nodeId);
             if (node) {
-                html.push('<li>');
+                html.push('<li id="li_' + node.nodeId + '">');
                 html.push(view.getLineHtml(node));
                 html.push(view.getChkHtml(node, this.options));
-                html.push('<a href="javascript:void(0);">');
+                html.push('<a href="javascript:void(0);" id="a_' + node.nodeId + '" nId="' + node.nodeId + '">');
                 html.push(view.getIconHtml(node));
                 html.push('<span>' + node.text + '</span>');
                 html.push(view.getOperateHtml(node, this.options));
@@ -3053,6 +3088,41 @@ define('kotenei/tree', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
 
     Tree.prototype.removeNode = function (node) {
         delete this.nodes[this.prefix + node.id];
+    };
+
+    //获取父节点
+    Tree.prototype.getPCNodes = function (node) {
+        var parentNode = this.nodes[this.prefix + node.parentId];
+        var nodes = [];
+
+        while (parentNode) {
+            nodes.push(parentNode);
+            parentNode = this.nodes[this.prefix + parentNode.parentId];
+        }
+
+        this.getChildNodes(node, nodes);
+
+        return nodes;
+    };
+
+    //获取子节点
+    Tree.prototype.getChildNodes = function (node, nodes) {
+        if (!utils.isArray(node.nodes)) {
+            return;
+        }
+
+        for (var i = 0; i < node.nodes.length; i++) {
+            nodes.push(node.nodes[i]);
+            this.getChildNodes(node.nodes[i], nodes);
+        }
+
+    };
+
+    Tree.prototype.getSelected = function () {
+        var $selected = this.$element.find('a.selected');
+        if ($selected.length === 0) { return null; }
+        var id = $selected.attr('nId');
+        return this.getNode(id);
     };
 
     Tree.prototype.getNode = function (id) {
