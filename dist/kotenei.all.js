b08235e1399113fc0b31474fc9d095a809e2e612
kotenei <kotenei@sina.com>
2014-10-31 15:52:07 +0800

    update highlight

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -1531,10 +1531,17 @@ function($, Dropdown, util){
 define('kotenei/highlight', ['jquery'], function ($) {
 
     var exports = {};
-
     var $body = $(document.body);
+    var defaultClass = "k-highlight";
 
-    exports.highlight = function ($elm, keywords, className) {
+    /**
+     * 高亮HTML内容
+     * @param  {JQuery} $elm - dom
+     * @param  {String|Array} keywords - 需要高亮的关键字
+     * @param  {String} className - 高亮样式
+     * @return {Void}
+     */
+    exports.highlightHtml = function ($elm, keywords, className) {
 
         if ($.isArray($elm)) {
             className = keywords;
@@ -1542,27 +1549,64 @@ define('kotenei/highlight', ['jquery'], function ($) {
             $elm = $body;
         }
 
+        var html = this.highlightText($elm.html(), keywords, className);
+        $elm.html(html);
+    };
+
+    /**
+     * 高亮文本
+     * @param  {String} Source - 原字符串
+     * @param  {String|Array} keywords - 需要高亮的关键字
+     * @param  {String} className - 高亮样式
+     * @return {String}
+     */
+    exports.highlightText = function (source, keywords, className) {
+
+        if (!source || source.length === 0) {
+            return '';
+        }
+
+        source = this.highlightClean(source, className);
+        className = className || defaultClass;
+
+
+        if (!keywords) {
+            return source;
+        }
+
         if (!$.isArray(keywords)) {
             keywords = [keywords];
         }
 
-        className = className || "k-highlight"
+        if (keywords.length === 1 && $.trim(keywords[0]).length === 0) {
+            return source;
+        }
 
-        var html = this.highlightText($elm.html(), keywords, className);
+        var matches = source.match(/[^<>]+|<(\/?)([A-Za-z]+)([^<>]*)>/g);
+        for (var i = 0; i < matches.length; i++) {
+            if (!/<[^>]+>/.test(matches[i]) && $.trim(matches[i]).length != 0) {
+                matches[i] = matches[i].replace(new RegExp('(' + keywords.join('|') + ')', 'ig'), '<mark class="' + className + '">$1</mark>');
+            }
+        }
 
-        $elm.html(html);
+        return matches.join('');
     };
 
-    exports.highlightText = function (source, keywords, className) {
-        for (var i = 0,keyword; i < keywords.length; i++) {
-            keyword = keywords[i];
-            source = source.replace(new RegExp('('+keyword+")","igm"), '<mark class="'+className+'">$1</mark>');
-        }
+    /**
+     * 清除带高亮标签的文本并返回
+     * @param  {String} Source - 原字符串
+     * @param  {String} className - 高亮样式
+     * @return {String}
+     */
+    exports.highlightClean = function (source, className) {
+
+        className = className || defaultClass;
+
+        var reg = new RegExp('<mark class="?' + className + '"?>(.*?)<\/mark>', 'ig');
+
+        source = source.replace(reg, '$1');
+
         return source;
-    }
-
-    exports.clean = function (source) {
-
     };
 
     return exports;
