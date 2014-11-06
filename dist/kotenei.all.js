1164eedf0ee92b240874b4ab7785d095539b35cf
kotenei <kotenei@sina.com>
2014-11-06 18:01:19 +0800

    update

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -2441,29 +2441,91 @@ define('kotenei/imgPreview', ['jquery', 'kotenei/loading', 'kotenei/popTips'], f
 
     var ImgPreview = function ($elements, options) {
         this.$elements = $elements;
-        this.options = $.extend({
+        this.options = $.extend({}, {
             showButtons: true,
             backdrop: true,
             backdropClose: true,
             tpl: '<div class="k-imgPreview">' +
-                    '<div class="container"></div>' +
+                    '<div class="container">' +
+                        '<span class="close"><i class="fa fa-close"></i></span>' +
+                        '<span class="prev"><i class="fa fa-chevron-left"></i></span>' +
+                        '<span class="next"><i class="fa fa-chevron-right"></i></span>' +
+                        '<img src="" />' +
+                    '</div>' +
                     '<p class="description"></p>' +
                  '</div>'
-        });
-        this.imgs = {};
+        }, options);
+        this.index = 0;
         this.init();
     };
 
     ImgPreview.prototype.init = function () {
         var self = this;
         if (this.$elements.length === 0) { return; }
+
         this.$imgPreview = $(this.options.tpl).appendTo(document.body);
+        this.$backdrop = $('<div/>').addClass('k-imgPreview-backdrop').appendTo(document.body);
+
         this.$elements.css('cursor', 'pointer').each(function (i) {
-            var id = 'imgPreview_' + i;
+            var $this = $(this),
+                id = 'imgPreview_' + i;
             this.id = id;
-            self.imgs[id] = $(this);
+            $this.attr('data-index', i);
         });
 
+        this.$elements.on('click', function () {
+            var $this = $(this),
+                src = $this.attr('data-href') || $this.attr('src');
+
+            if (!src) {
+                return;
+            }
+
+            Loading.show();
+            self.imgLoad(src, $this, function (result) {
+                if (result) {
+                    self.$imgPreview.find('img').attr('src', src);
+                    self.show();
+                } else {
+                    self.hide();
+                }
+                Loading.hide();
+            });
+        });
+
+        this.$backdrop.on('click', function () {
+            if (self.options.backdropClose) {
+                self.hide();
+            }
+        });
+    };
+
+    ImgPreview.prototype.imgLoad = function (src, $img, callback) {
+        var img = new Image();
+
+        img.onload = function () {
+            $img.attr('src', src);
+            callback(true);
+        };
+        img.onerror = function () {
+            popTips.error('图片加载失败！', 1500);
+            callback(false);
+        };
+        img.src = src;
+    };
+
+    ImgPreview.prototype.show = function () {
+        this.$imgPreview.fadeIn('fast');
+        if (this.options.backdrop) {
+            this.$backdrop.show();
+        }
+    };
+
+    ImgPreview.prototype.hide = function () {
+        this.$imgPreview.fadeOut('fast');
+        if (this.options.backdrop) {
+            this.$backdrop.hide();
+        }
     };
 
     return ImgPreview;
