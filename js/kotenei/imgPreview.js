3198c0e9f179378de1b283c4cef9dd4bfeca45f1
kotenei <kotenei@sina.com>
2014-11-07 21:58:13 +0800

    更新图片预览

--- js/kotenei/imgPreview.js
+++ js/kotenei/imgPreview.js
@@ -6,31 +6,48 @@
 /*global define, require, document*/
 define('kotenei/imgPreview', ['jquery', 'kotenei/loading', 'kotenei/popTips'], function ($, Loading, popTips) {
 
+    /**
+     * 图片预览模块
+     * @constructor
+     * @alias kotenei/imgPreview
+     * @param {JQuery} $elements - dom
+     * @param {Object} options - 参数设置
+     */
     var ImgPreview = function ($elements, options) {
         this.$elements = $elements;
         this.options = $.extend({}, {
+            delay:500,
             showButtons: true,
             backdrop: true,
             backdropClose: true,
             tpl: '<div class="k-imgPreview">' +
                     '<div class="container">' +
-                        '<span class="close"><i class="fa fa-close"></i></span>' +
-                        '<span class="prev"><i class="fa fa-chevron-left"></i></span>' +
-                        '<span class="next"><i class="fa fa-chevron-right"></i></span>' +
+                        '<span class="close" role="close"><i class="fa fa-close"></i></span>' +
+                        '<span class="prev" role="prev"><i class="fa fa-chevron-left"></i></span>' +
+                        '<span class="next" role="next"><i class="fa fa-chevron-right"></i></span>' +
                         '<img src="" />' +
                     '</div>' +
                     '<p class="description"></p>' +
                  '</div>'
         }, options);
+        this.$win = $(window);
+        this.isLoading = false;
         this.index = 0;
         this.init();
     };
 
+    /**
+     * 初始化
+     * @return {Void}   
+     */
     ImgPreview.prototype.init = function () {
-        var self = this;
         if (this.$elements.length === 0) { return; }
 
         this.$imgPreview = $(this.options.tpl).appendTo(document.body);
+        this.$container = this.$imgPreview.find('.container');
+        this.$img = this.$container.find('img');
+        this.$prev = this.$container.find('.prev');
+        this.$next = this.$container.find('.next');
         this.$backdrop = $('<div/>').addClass('k-imgPreview-backdrop').appendTo(document.body);
 
         this.$elements.css('cursor', 'pointer').each(function (i) {
@@ -40,24 +57,20 @@ define('kotenei/imgPreview', ['jquery', 'kotenei/loading', 'kotenei/popTips'], f
             $this.attr('data-index', i);
         });
 
+        this.eventBind();
+    };
+
+    /**
+     * 事件绑定
+     * @return {Void}   
+     */
+    ImgPreview.prototype.eventBind = function () {
+        var self = this;
+
         this.$elements.on('click', function () {
-            var $this = $(this),
-                src = $this.attr('data-href') || $this.attr('src');
-
-            if (!src) {
-                return;
-            }
-
-            Loading.show();
-            self.imgLoad(src, $this, function (result) {
-                if (result) {
-                    self.$imgPreview.find('img').attr('src', src);
-                    self.show();
-                } else {
-                    self.hide();
-                }
-                Loading.hide();
-            });
+            var $this = $(this);
+            self.index = Number($this.attr('data-index'));
+            self.show();
         });
 
         this.$backdrop.on('click', function () {
@@ -65,34 +78,194 @@ define('kotenei/imgPreview', ['jquery', 'kotenei/loading', 'kotenei/popTips'], f
                 self.hide();
             }
         });
+
+        this.$imgPreview.on('click', '[role=close]', function () {
+            //关闭
+            self.hide();
+        }).on('click', '[role=prev]', function () {
+            //向前
+            if (self.index === 0) {
+                return;
+            }
+            self.index--;
+            self.showControls();
+            self.show();
+        }).on('click', '[role=next]', function () {
+            //向后
+            if (self.index >= self.$elements.length - 1) {
+                return;
+            }
+            self.index++;
+            self.showControls();
+            self.show();
+        }).on('mouseenter', function () {
+            self.showControls();
+        }).on('mouseleave', function () {
+            self.$prev.hide();
+            self.$next.hide();
+        });
     };
 
-    ImgPreview.prototype.imgLoad = function (src, $img, callback) {
+    /**
+     * 按钮显示
+     * @return {Void}   
+     */
+    ImgPreview.prototype.showControls = function () {
+        var len = this.$elements.length;
+
+        if (len === 1) {
+            return;
+        }
+
+        if (this.index === 0) {
+            this.$prev.hide();
+            this.$next.show();
+        } else if (this.index >= len - 1) {
+            this.$prev.show();
+            this.$next.hide();
+        } else {
+            this.$prev.show();
+            this.$next.show();
+        }
+    };
+
+    /**
+     * 图片加载
+     * @param {String} str - 图片路径
+     * @param {Function} callback - 回调函数
+     * @return {Void}   
+     */
+    ImgPreview.prototype.imgLoad = function (src, callback) {
         var img = new Image();
 
         img.onload = function () {
-            $img.attr('src', src);
-            callback(true);
+            callback(true, { width: img.width, height: img.height });
         };
         img.onerror = function () {
             popTips.error('图片加载失败！', 1500);
-            callback(false);
+            callback(false, { width: 0, height: 0 });
         };
         img.src = src;
     };
 
+    /**
+     * 获取窗体最大尺寸
+     * @return {Object}   
+     */
+    ImgPreview.prototype.getMaxSize = function () {
+        return {
+            width: this.$win.width() - 100,
+            height: this.$win.height() - 100
+        };
+    };
+
+    /**
+     * 缩放
+     * @param {Number} width - 宽度
+     * @param {Number} height - 高度
+     * @return {Object}   
+     */
+    ImgPreview.prototype.zoom = function (width, height) {
+
+        var ratio;
+        var nW, nh;
+        var maxSize = this.getMaxSize();
+
+        if (width < maxSize.width && height < maxSize.height) {
+            return { width: width, height: height };
+        }
+
+        if (width >= height) {
+            ratio = width / height;
+            nh = maxSize.width / ratio;
+        } else {
+            ratio = height / width;
+            nh = maxSize.width * ratio;
+        }
+
+        nw = maxSize.width;
+
+        if (nh > maxSize.height) {
+            nh = maxSize.height;
+            nw = width / height * nh;
+        }
+
+        return { width: nw, height: nh };
+    };
+
+    /**
+     * 显示
+     * @return {Void}   
+     */
     ImgPreview.prototype.show = function () {
-        this.$imgPreview.fadeIn('fast');
+        var self = this,
+            $img = this.$elements.eq(this.index),
+            src;
+
+        if ($img.length === 0) { return; }
+
+        src = $img.attr('data-href') || $img.attr('src');
+
+        if (!src || this.isLoading) { return; }
+
+        this.isLoading = true;
+
+        Loading.show();
+
+        this.imgLoad(src, function (result, size) {
+            if (result) {
+                self.$img.attr('src', src);
+                self.setPosition(size);
+            } else {
+                self.hide();
+            }
+            Loading.hide();
+            self.isLoading = false;
+        });
+    };
+
+    /**
+     * 隐藏
+     * @return {Void}   
+     */
+    ImgPreview.prototype.hide = function () {
+        this.$imgPreview.fadeOut(this.options.delay);
         if (this.options.backdrop) {
-            this.$backdrop.show();
+            this.$backdrop.fadeOut(this.options.delay);
         }
     };
 
-    ImgPreview.prototype.hide = function () {
-        this.$imgPreview.fadeOut('fast');
+    /**
+     * 定位
+     * @param {Object} imgSize - 图片尺寸
+     * @return {Void}   
+     */
+    ImgPreview.prototype.setPosition = function (imgSize) {
+
+        var win_w = this.$win.width(),
+            win_h = this.$win.height(),
+            iw = imgSize.width,
+            ih = imgSize.height;
+
+        var size = this.zoom(iw, ih);
+
+
+        this.$container.css({
+            width: size.width,
+            height: size.height
+        });
+
+        this.$imgPreview.hide().css({
+            width: size.width + 20,
+            height: size.height + 20,
+            marginTop: - ((size.height + 20) / 2),
+            marginLeft:  - ((size.width + 20) / 2)
+        }).fadeIn(this.options.delay);
+
         if (this.options.backdrop) {
-            this.$backdrop.hide();
+            this.$backdrop.fadeIn(this.options.delay);
         }
+
     };
 
     return ImgPreview;
