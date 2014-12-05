f4141c8df3ff50d84030c8a789b93e9e55cdef64
kotenei <kotenei@qq.com>
2014-12-05 11:41:12 +0800

    添加ajax

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -1,3 +1,126 @@
+/*
+ * ajax 模块
+ * @date:2014-12-05
+ * @author:kotenei(kotenei@qq.com)
+ */
+define('kotenei/ajax', ['jquery', 'kotenei/loading', 'kotenei/popTips'], function ($, Loading, popTips) {
+
+    /**
+     * ajax 通用操作封装
+     * @return {Object} 
+     */
+    var Ajax = (function () {
+
+        var _instance;
+
+        function init() {
+
+            /**
+            * ajax 返回数据类型约定： String([JSON,HTML]) || Object(JSON)
+            * json 字符串或json对象须包含以下参数，属性大写开头:
+            * {
+            *     Status:Boolean,           （操作成功与否）
+            *     Message:String|Null,      （操作成功提示信息）
+            *     ErrorMessage:String/Null, （操作失败错误信息）
+            *     Data:Object|Null          （返回的数据对象）
+            * }
+            */
+            var ajax = function (type, url, data) {
+
+                var dtd = $.Deferred();
+
+                Loading.show();
+
+                $.ajax({
+                    url: url,
+                    type: type,
+                    data: data || {}
+                }).done(function (ret) {
+                    if (typeof ret === 'string') {
+                        try {
+                            ret = eval('(' + ret + ')');
+                        } catch (e) {
+                            dtd.resolve(ret);
+                            return dtd.promise();
+                        }
+                    }
+                    if (ret.Status) {
+                        if (ret.Message) {
+                            popTips.success(ret.Message, 1000, function () {
+                                if (ret.Url) {
+                                    window.location.href = ret.Url;
+                                } else {
+                                    dtd.resolve(ret);
+                                }
+                            });
+                        } else if (ret.Url) {
+                            window.location.href = ret.Url;
+                        } else {
+                            dtd.resolve(ret);
+                        }
+                    } else {
+                        if (ret.ErrorMessage) {
+                            popTips.error(ret.ErrorMessage || "发生了未知错误", 1000, function () {
+                                if (ret.Url) {
+                                    window.location.href = ret.url;
+                                } else {
+                                    dtd.resolve(ret);
+                                }
+                            })
+                        } else if (ret.Url) {
+                            window.location.href = ret.Url;
+                        } else {
+                            dtd.resolve(ret);
+                        }
+                    }
+                }).fail(function () {
+                    popTips.error("服务器发生错误", 1000);
+                }).always(function () {
+                    Loading.hide();
+                });
+
+                return dtd.promise();
+            };
+
+            return {
+                post: function (url, data) {
+                    return ajax("POST", url, data);
+                },
+                get: function (url, data) {
+                    return ajax("GET", url, data);
+                },
+                ajaxForm: function ($form) {
+                    var url = $form.attr('action'),
+                        type = $form.attr('method'),
+                        data = $form.serialize();
+
+                    if ($form.valid) {
+                        if ($form.valid()) {
+                            return ajax(type, url, data);
+                        }
+                    } else {
+                        return ajax(type, url, data);
+                    }
+
+                }
+            };
+
+        };
+
+        return {
+            getInstance: function () {
+                if (!_instance) {
+                    _instance = init();
+                }
+
+                return _instance;
+            }
+        }
+
+    })();
+
+    return Ajax.getInstance();
+});
 /**
  * @module kotenei/app 
  * @author kotenei (kotenei@qq.com)
@@ -6302,8 +6425,9 @@ define('kotenei/wordLimit', ['jquery'], function ($) {
     return WordLimit;
 });
 ;
-define("kotenei", ["kotenei/app", "kotenei/autoComplete", "kotenei/cache", "kotenei/clipZoom", "kotenei/datepicker", "kotenei/dragdrop", "kotenei/dropdown", "kotenei/dropdownDatepicker", "kotenei/event", "kotenei/highlight", "kotenei/imgPreview", "kotenei/infiniteScroll", "kotenei/lazyload", "kotenei/loading", "kotenei/pager", "kotenei/placeholder", "kotenei/popover", "kotenei/popTips", "kotenei/router", "kotenei/slider", "kotenei/switch", "kotenei/tooltips", "kotenei/tree", "kotenei/util", "kotenei/validate", "kotenei/validateTooltips", "kotenei/waterfall", "kotenei/window", "kotenei/wordLimit"], function(_app, _autoComplete, _cache, _clipZoom, _datepicker, _dragdrop, _dropdown, _dropdownDatepicker, _event, _highlight, _imgPreview, _infiniteScroll, _lazyload, _loading, _pager, _placeholder, _popover, _popTips, _router, _slider, _switch, _tooltips, _tree, _util, _validate, _validateTooltips, _waterfall, _window, _wordLimit){
+define("kotenei", ["kotenei/ajax", "kotenei/app", "kotenei/autoComplete", "kotenei/cache", "kotenei/clipZoom", "kotenei/datepicker", "kotenei/dragdrop", "kotenei/dropdown", "kotenei/dropdownDatepicker", "kotenei/event", "kotenei/highlight", "kotenei/imgPreview", "kotenei/infiniteScroll", "kotenei/lazyload", "kotenei/loading", "kotenei/pager", "kotenei/placeholder", "kotenei/popover", "kotenei/popTips", "kotenei/router", "kotenei/slider", "kotenei/switch", "kotenei/tooltips", "kotenei/tree", "kotenei/util", "kotenei/validate", "kotenei/validateTooltips", "kotenei/waterfall", "kotenei/window", "kotenei/wordLimit"], function(_ajax, _app, _autoComplete, _cache, _clipZoom, _datepicker, _dragdrop, _dropdown, _dropdownDatepicker, _event, _highlight, _imgPreview, _infiniteScroll, _lazyload, _loading, _pager, _placeholder, _popover, _popTips, _router, _slider, _switch, _tooltips, _tree, _util, _validate, _validateTooltips, _waterfall, _window, _wordLimit){
     return {
+        "Ajax" : _ajax,
         "App" : _app,
         "AutoComplete" : _autoComplete,
         "cache" : _cache,
