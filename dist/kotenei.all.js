c6379a6e6800b760811279fe6170bd0964e3dc03
kotenei <kotenei@qq.com>
2015-07-16 14:26:16 +0800

    update

--- dist/kotenei.all.js
+++ dist/kotenei.all.js
@@ -4003,80 +4003,158 @@ define('kotenei/loading', ['jquery', 'spin'], function ($, Spinner) {
  * @date:2015-07-15
  * @author:kotenei(kotenei@qq.com)
  */
-define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
+define('kotenei/magnifier', ['jquery'], function ($) {
 
+    /**
+     * 放大镜模块
+     * @constructor
+     * @alias kotenei/imgPreview
+     * @param {JQuery} $el - dom
+     * @param {Object} options - 参数设置
+     */
     var Magnifier = function ($el, options) {
         this.options = $.extend({}, {
             offset: 10,
             width: 400,
-            height:400,
-            selector: {
-                width: 150,
-                height: 150
-            }
+            height: 250,
+            zoomWidth: 400,
+            zoomHeight: 400,
+            zIndex: 100
         }, options);
         this.$el = $el;
-        this._isCreate = false;
+        this._isLoading = false;
         this.init();
     };
 
+    /**
+     * 初始化
+     * @return {Void}   
+     */
     Magnifier.prototype.init = function () {
         this.create();
         this.watch();
     };
 
+    /**
+     * 事件监控
+     * @return {Void}   
+     */
     Magnifier.prototype.watch = function () {
         var self = this;
+       
 
         this.$el.on('mousemove', function (e) {
+            var src = self.$img.attr('data-big-img');
+
             self.$view.show();
-            self.$selector.show();
-            self.setPosition(e);
+
+            if (self.$viewImg.attr('data-isLoad') == 'true') {
+                self.$selector.show();
+                self.setPosition(e);
+            } else {
+
+                self.$viewImg.hide();
+
+                if (self._isLoading) {
+                    return;
+                }
+
+                self._isLoading = true;
+
+                self.loadImg(src, function (success, size) {
+
+                    self.$viewLoading.hide();
+
+                    self._isLoading = false;
+
+                    self.$viewImg.attr({
+                        'src': src,
+                        'data-isLoad': true
+                    }).show();
+
+                    self.$selector.width(self.options.zoomWidth / (size.width / self.options.width))
+                                  .height(self.options.zoomHeight / (size.height / self.options.height));
+                    
+                });
+            }
+
+
         }).on('mouseleave', function (e) {
             self.$view.hide();
             self.$selector.hide();
         });
     };
 
+    /**
+     * 创建
+     * @return {Void}   
+     */
     Magnifier.prototype.create = function () {
+
+        this.$el.width(this.options.width).height(this.options.height);
 
         this.$imgBox = this.$el.find('.k-magnifier-imgbox').css({
             width: this.options.width,
             height: this.options.height
         });
 
-        this.$view = $('<div class="k-magnifier-view"><img src="../images/big.jpg" /></div>')
+        this.$img = this.$el.find('.k-magnifier-imgbox img');
+
+        this.$view = $('<div class="k-magnifier-view"><img src="" /><div class="k-magnifier-loading"><span class="fa fa-spinner">asfdasfa</span><br/>图片加载中，请稍候...</div></div>')
             .appendTo(this.$el)
             .css({
-                width: this.options.width,
-                height: this.options.height,
+                width: this.options.zoomWidth,
+                height: this.options.zoomHeight,
                 left: this.$imgBox.position().left + this.options.width + this.options.offset,
-                top:this.$imgBox.position().top
+                top: this.$imgBox.position().top,
+                zIndex:this.options.zIndex
             });
-            
 
+        this.$viewLoading = this.$view.find('.k-magnifier-loading');
         this.$viewImg = this.$view.find('img');
 
         this.$selector = $('<div class="k-magnifier-selector"></div>')
             .appendTo(this.$imgBox)
             .css({
-                width: this.options.selector.width - 2,
-                height: this.options.selector.height - 2
+                left: 0,
+                top:0
             });
     };
 
+    /**
+     * 加载图片
+     * @return {Void}   
+     */
+    Magnifier.prototype.loadImg = function (src, callback) {
+        var img = new Image();
+
+        img.onload = function () {
+            callback(true, { width: img.width, height: img.height });
+        };
+        img.onerror = function () {
+            callback(false, { width: 0, height: 0 });
+        };
+
+        img.src = src;
+    };
+
+    /**
+     * 设置位置
+     * @return {Void}   
+     */
     Magnifier.prototype.setPosition = function (e) {
         var x = e.pageX,
             y = e.pageY,
             left = x - this.$el.offset().left,
             top = y - this.$el.offset().top,
-            maxLeft = this.$el.width() - this.options.selector.width,
-            maxTop = this.$el.height() - this.options.selector.height,
+            selectorWidth = this.$selector.outerWidth(),
+            selectorHeight = this.$selector.outerHeight(),
+            maxLeft = this.options.width - selectorWidth,
+            maxTop = this.options.height - selectorHeight,
             percentX, percentY;
 
-        left = left - this.options.selector.width / 2;
-        top = top - this.options.selector.height / 2;
-
+        left = left - selectorWidth / 2;
+        top = top - selectorHeight / 2;
 
         if (left < 0) {
             left = 0;
@@ -4090,8 +4168,10 @@ define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDro
             top = maxTop;
         }
 
-        percentX = left / (this.$el.width() - this.options.selector.width);
-        percentY = top / (this.$el.height() - this.options.selector.height);
+
+
+        percentX = left / (this.$el.width() - selectorWidth);
+        percentY = top / (this.$el.height() - selectorHeight);
 
 
         this.$selector.css({
@@ -4101,8 +4181,8 @@ define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDro
 
 
         this.$viewImg.css({
-            width: this.options.width/this.options.selector.width*this.options.width,
-            height: this.options.height / this.options.selector.height * this.options.height,
+            width: this.options.zoomWidth / selectorWidth * this.options.width,
+            height: this.options.zoomHeight / selectorHeight * this.options.height,
             left: -percentX * (this.$viewImg.width() - this.$view.width()),
             top: -percentY * (this.$viewImg.height() - this.$view.height())
         })
@@ -4384,6 +4464,89 @@ define('kotenei/placeholder', ['jquery'], function($) {
     }
 });
 /*
+ * 弹出提示模块
+ * @date:2014-09-10
+ * @author:kotenei(kotenei@qq.com)
+ */
+define('kotenei/popTips', ['jquery'], function ($) {
+
+    /**
+     * 弹出提示模块
+     * @return {Object} 
+     */
+    var PopTips = (function () {
+
+        var _instance;
+
+        function init() {
+
+            var $tips, tm;
+
+            function build(status, content, delay, callback) {
+
+                if (tm) { clearTimeout(tm); }
+
+                if ($.isFunction(delay)) { callback = delay; delay = 3000; }
+
+                callback = callback || $.noop;
+                delay = delay || 3000;
+
+                if ($tips) { $tips.stop().remove(); }
+
+                $tips = $(getHtml(status, content))
+                        .appendTo(document.body).hide();
+
+                $tips.css({ marginLeft: -($tips.width() / 2), marginTop: -($tips.height() / 2) }).fadeIn('fase', function () {
+                    tm = setTimeout(function () {
+                        $tips.stop().remove();
+                        callback();
+                    }, delay);
+                })
+            }
+
+            function getHtml(status, content) {
+                var html = [];
+                switch (status) {
+                    case "success":
+                        html.push('<div class="k-pop-tips success"><span class="fa fa-check"></span>&nbsp;<span>' + content + '</span></div>');
+                        break;
+                    case "error":
+                        html.push('<div class="k-pop-tips error"><span class="fa fa-close"></span>&nbsp;<span>' + content + '</span></div>');
+                        break;
+                    case "warning":
+                        html.push('<div class="k-pop-tips warning"><span class="fa fa-exclamation"></span>&nbsp;<span>' + content + '</span></div>');
+                        break;
+                }
+                return html.join('');
+            }
+
+            return {
+                success: function (content, callback, delay) {
+                    build("success", content, callback, delay);
+                },
+                error: function (content, callback, delay) {
+                    build("error", content, callback, delay);
+                },
+                warning: function (content, callback, delay) {
+                    build("warning", content, callback, delay);
+                }
+            };
+        }
+
+        return {
+            getInstance: function () {
+                if (!_instance) {
+                    _instance = init();
+                }
+                return _instance;
+            }
+        }
+    })();
+
+    return PopTips.getInstance();
+});
+
+/*
  * 弹出框模块
  * @date:2014-11-05
  * @author:kotenei(kotenei@qq.com)
@@ -4485,89 +4648,6 @@ define('kotenei/popover', ['jquery', 'kotenei/tooltips', 'kotenei/util'], functi
 
     return Popover;
 });
-/*
- * 弹出提示模块
- * @date:2014-09-10
- * @author:kotenei(kotenei@qq.com)
- */
-define('kotenei/popTips', ['jquery'], function ($) {
-
-    /**
-     * 弹出提示模块
-     * @return {Object} 
-     */
-    var PopTips = (function () {
-
-        var _instance;
-
-        function init() {
-
-            var $tips, tm;
-
-            function build(status, content, delay, callback) {
-
-                if (tm) { clearTimeout(tm); }
-
-                if ($.isFunction(delay)) { callback = delay; delay = 3000; }
-
-                callback = callback || $.noop;
-                delay = delay || 3000;
-
-                if ($tips) { $tips.stop().remove(); }
-
-                $tips = $(getHtml(status, content))
-                        .appendTo(document.body).hide();
-
-                $tips.css({ marginLeft: -($tips.width() / 2), marginTop: -($tips.height() / 2) }).fadeIn('fase', function () {
-                    tm = setTimeout(function () {
-                        $tips.stop().remove();
-                        callback();
-                    }, delay);
-                })
-            }
-
-            function getHtml(status, content) {
-                var html = [];
-                switch (status) {
-                    case "success":
-                        html.push('<div class="k-pop-tips success"><span class="fa fa-check"></span>&nbsp;<span>' + content + '</span></div>');
-                        break;
-                    case "error":
-                        html.push('<div class="k-pop-tips error"><span class="fa fa-close"></span>&nbsp;<span>' + content + '</span></div>');
-                        break;
-                    case "warning":
-                        html.push('<div class="k-pop-tips warning"><span class="fa fa-exclamation"></span>&nbsp;<span>' + content + '</span></div>');
-                        break;
-                }
-                return html.join('');
-            }
-
-            return {
-                success: function (content, callback, delay) {
-                    build("success", content, callback, delay);
-                },
-                error: function (content, callback, delay) {
-                    build("error", content, callback, delay);
-                },
-                warning: function (content, callback, delay) {
-                    build("warning", content, callback, delay);
-                }
-            };
-        }
-
-        return {
-            getInstance: function () {
-                if (!_instance) {
-                    _instance = init();
-                }
-                return _instance;
-            }
-        }
-    })();
-
-    return PopTips.getInstance();
-});
-
 /**
  * 路由
  * @date :2014-09-21
--- js/kotenei/magnifier.js
+++ js/kotenei/magnifier.js
@@ -3,80 +3,158 @@
  * @date:2015-07-15
  * @author:kotenei(kotenei@qq.com)
  */
-define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDrop) {
+define('kotenei/magnifier', ['jquery'], function ($) {
 
+    /**
+     * 放大镜模块
+     * @constructor
+     * @alias kotenei/imgPreview
+     * @param {JQuery} $el - dom
+     * @param {Object} options - 参数设置
+     */
     var Magnifier = function ($el, options) {
         this.options = $.extend({}, {
             offset: 10,
             width: 400,
-            height:400,
-            selector: {
-                width: 150,
-                height: 150
-            }
+            height: 250,
+            zoomWidth: 400,
+            zoomHeight: 400,
+            zIndex: 100
         }, options);
         this.$el = $el;
-        this._isCreate = false;
+        this._isLoading = false;
         this.init();
     };
 
+    /**
+     * 初始化
+     * @return {Void}   
+     */
     Magnifier.prototype.init = function () {
         this.create();
         this.watch();
     };
 
+    /**
+     * 事件监控
+     * @return {Void}   
+     */
     Magnifier.prototype.watch = function () {
         var self = this;
+       
 
         this.$el.on('mousemove', function (e) {
+            var src = self.$img.attr('data-big-img');
+
             self.$view.show();
-            self.$selector.show();
-            self.setPosition(e);
+
+            if (self.$viewImg.attr('data-isLoad') == 'true') {
+                self.$selector.show();
+                self.setPosition(e);
+            } else {
+
+                self.$viewImg.hide();
+
+                if (self._isLoading) {
+                    return;
+                }
+
+                self._isLoading = true;
+
+                self.loadImg(src, function (success, size) {
+
+                    self.$viewLoading.hide();
+
+                    self._isLoading = false;
+
+                    self.$viewImg.attr({
+                        'src': src,
+                        'data-isLoad': true
+                    }).show();
+
+                    self.$selector.width(self.options.zoomWidth / (size.width / self.options.width))
+                                  .height(self.options.zoomHeight / (size.height / self.options.height));
+                    
+                });
+            }
+
+
         }).on('mouseleave', function (e) {
             self.$view.hide();
             self.$selector.hide();
         });
     };
 
+    /**
+     * 创建
+     * @return {Void}   
+     */
     Magnifier.prototype.create = function () {
+
+        this.$el.width(this.options.width).height(this.options.height);
 
         this.$imgBox = this.$el.find('.k-magnifier-imgbox').css({
             width: this.options.width,
             height: this.options.height
         });
 
-        this.$view = $('<div class="k-magnifier-view"><img src="../images/big.jpg" /></div>')
+        this.$img = this.$el.find('.k-magnifier-imgbox img');
+
+        this.$view = $('<div class="k-magnifier-view"><img src="" /><div class="k-magnifier-loading"><span class="fa fa-spinner">asfdasfa</span><br/>图片加载中，请稍候...</div></div>')
             .appendTo(this.$el)
             .css({
-                width: this.options.width,
-                height: this.options.height,
+                width: this.options.zoomWidth,
+                height: this.options.zoomHeight,
                 left: this.$imgBox.position().left + this.options.width + this.options.offset,
-                top:this.$imgBox.position().top
+                top: this.$imgBox.position().top,
+                zIndex:this.options.zIndex
             });
-            
 
+        this.$viewLoading = this.$view.find('.k-magnifier-loading');
         this.$viewImg = this.$view.find('img');
 
         this.$selector = $('<div class="k-magnifier-selector"></div>')
             .appendTo(this.$imgBox)
             .css({
-                width: this.options.selector.width - 2,
-                height: this.options.selector.height - 2
+                left: 0,
+                top:0
             });
     };
 
+    /**
+     * 加载图片
+     * @return {Void}   
+     */
+    Magnifier.prototype.loadImg = function (src, callback) {
+        var img = new Image();
+
+        img.onload = function () {
+            callback(true, { width: img.width, height: img.height });
+        };
+        img.onerror = function () {
+            callback(false, { width: 0, height: 0 });
+        };
+
+        img.src = src;
+    };
+
+    /**
+     * 设置位置
+     * @return {Void}   
+     */
     Magnifier.prototype.setPosition = function (e) {
         var x = e.pageX,
             y = e.pageY,
             left = x - this.$el.offset().left,
             top = y - this.$el.offset().top,
-            maxLeft = this.$el.width() - this.options.selector.width,
-            maxTop = this.$el.height() - this.options.selector.height,
+            selectorWidth = this.$selector.outerWidth(),
+            selectorHeight = this.$selector.outerHeight(),
+            maxLeft = this.options.width - selectorWidth,
+            maxTop = this.options.height - selectorHeight,
             percentX, percentY;
 
-        left = left - this.options.selector.width / 2;
-        top = top - this.options.selector.height / 2;
-
+        left = left - selectorWidth / 2;
+        top = top - selectorHeight / 2;
 
         if (left < 0) {
             left = 0;
@@ -90,8 +168,10 @@ define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDro
             top = maxTop;
         }
 
-        percentX = left / (this.$el.width() - this.options.selector.width);
-        percentY = top / (this.$el.height() - this.options.selector.height);
+
+
+        percentX = left / (this.$el.width() - selectorWidth);
+        percentY = top / (this.$el.height() - selectorHeight);
 
 
         this.$selector.css({
@@ -101,8 +181,8 @@ define('kotenei/magnifier', ['jquery', 'kotenei/dragdrop'], function ($, DragDro
 
 
         this.$viewImg.css({
-            width: this.options.width/this.options.selector.width*this.options.width,
-            height: this.options.height / this.options.selector.height * this.options.height,
+            width: this.options.zoomWidth / selectorWidth * this.options.width,
+            height: this.options.zoomHeight / selectorHeight * this.options.height,
             left: -percentX * (this.$viewImg.width() - this.$view.width()),
             top: -percentY * (this.$viewImg.height() - this.$view.height())
         })
