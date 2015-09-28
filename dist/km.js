5b6d17defea0f3b1ba4199209bdf60bf9f35af62
kotenei <kotenei@qq.com>
2015-09-28 18:41:39 +0800

    update

--- dist/km.js
+++ dist/km.js
@@ -8037,6 +8037,151 @@ define('km/tagSelector', ['jquery'], function ($) {
 
 });
 /*
+ * 模板
+ * @date:2015-09-28
+ * @author:kotenei(kotenei@qq.com)
+ */
+define('km/template', ['jquery'], function ($) {
+
+    var index = 0;
+
+    var fileName = 'tpl_' + index;
+
+    var keywords =
+        'break,case,catch,continue,debugger,default,delete,do,else,false'
+      + ',finally,for,function,if,in,instanceof,new,null,return,switch,this'
+      + ',throw,true,try,typeof,var,void,while,with';
+
+    var tags = {
+        open: '{{',
+        close: '}}'
+    };
+
+
+    var util = {
+        each: function (arr, callback) {
+            for (var i = 0, item; i < arr.length; i++) {
+                item = arr[i];
+                if (callback(i, item) == false) {
+                    break;
+                }
+            }
+        }
+    };
+
+    var method = {
+
+        escape: function (code) {
+            return "'" + code
+            // 单引号与反斜杠转义
+            .replace(/('|\\)/g, '\\$1')
+            // 换行符转义(windows + linux)
+            .replace(/\r/g, '\\r')
+            .replace(/\n/g, '\\n') + "'";
+        },
+
+        html: function (code) {
+
+
+            code = code.replace(/\s+/g, ' ').replace(/<!--[\w\W]*?-->/g, '');
+
+            if (code) {
+                code = "$out+=" + method.escape(code) + ";" + "\n";
+            }
+
+
+            return code;
+        },
+        logic: function (code) {
+
+            code = method.parser(code);
+
+            console.log(code)
+
+            return code;
+
+        },
+        parser: function (code) {
+
+            code = code.replace(/^\s/, '');
+
+            var split = code.split(' ');
+            var key = split.shift();
+            var args = split.join(' ');
+
+            switch (key) {
+                case 'each':
+
+                    var item = split[0],
+                        data = split[2];
+
+                    code = "util.each(data,function(index," + item + "){";
+
+                    break;
+                case '/each':
+
+                    code = '})';
+
+                    break;
+                default:
+                    break;
+            }
+
+            return code;
+        }
+    };
+
+
+
+    var Template = function ($el, data) {
+        ++index;
+        this.$el = $el;
+        this.data = data;
+        this.source = this.$el.html().replace(/^\s*|\s*$/g, '');
+        this.fileName = fileName;
+        this.init();
+    };
+
+    Template.prototype.init = function () {
+
+        var mainCode = "$out='';";
+
+        util.each(this.source.split(tags.open), function (index, code) {
+            var code = code.split(tags.close);
+            var $c0 = code[0];
+            var $c1 = code[1];
+
+            if (code.length == 1) {
+                mainCode += method.html($c0);
+            } else {
+                mainCode += method.logic($c0);
+
+                if ($c1) {
+                    mainCode += method.html($c1);
+                }
+            }
+
+        });
+
+        //console.log(mainCode)
+
+    };
+
+    Template.prototype.compile = function () {
+
+    };
+
+    Template.prototype.render = function () {
+        var html = '';
+
+        return html;
+    };
+
+    return Template;
+
+});
+
+/*
  * 消息提示模块
  * @date:2014-09-05
  * @author:kotenei(kotenei@qq.com)
@@ -11373,7 +11518,7 @@ define('km/wordLimit', ['jquery'], function ($) {
     return WordLimit;
 });
 ;
-define("KM", ["km/ajax", "km/app", "km/autoComplete", "km/cache", "km/clipZoom", "km/contextMenu", "km/datePicker", "km/dragdrop", "km/dropdown", "km/dropdownDatepicker", "km/dropDownTree", "km/event", "km/focusMap", "km/highlight", "km/imgPreview", "km/infiniteScroll", "km/layout", "km/lazyload", "km/loading", "km/magnifier", "km/pager", "km/panel", "km/placeholder", "km/popover", "km/popTips", "km/rating", "km/resizable", "km/router", "km/slider", "km/switch", "km/tagSelector", "km/tooltips", "km/tree", "km/treeTable", "km/upload", "km/util", "km/validate", "km/validateTooltips", "km/waterfall", "km/window", "km/wordLimit"], function(_ajax, _app, _autoComplete, _cache, _clipZoom, _contextMenu, _datePicker, _dragdrop, _dropdown, _dropdownDatepicker, _dropDownTree, _event, _focusMap, _highlight, _imgPreview, _infiniteScroll, _layout, _lazyload, _loading, _magnifier, _pager, _panel, _placeholder, _popover, _popTips, _rating, _resizable, _router, _slider, _switch, _tagSelector, _tooltips, _tree, _treeTable, _upload, _util, _validate, _validateTooltips, _waterfall, _window, _wordLimit){
+define("KM", ["km/ajax", "km/app", "km/autoComplete", "km/cache", "km/clipZoom", "km/contextMenu", "km/datePicker", "km/dragdrop", "km/dropdown", "km/dropdownDatepicker", "km/dropDownTree", "km/event", "km/focusMap", "km/highlight", "km/imgPreview", "km/infiniteScroll", "km/layout", "km/lazyload", "km/loading", "km/magnifier", "km/pager", "km/panel", "km/placeholder", "km/popover", "km/popTips", "km/rating", "km/resizable", "km/router", "km/slider", "km/switch", "km/tagSelector", "km/template", "km/tooltips", "km/tree", "km/treeTable", "km/upload", "km/util", "km/validate", "km/validateTooltips", "km/waterfall", "km/window", "km/wordLimit"], function(_ajax, _app, _autoComplete, _cache, _clipZoom, _contextMenu, _datePicker, _dragdrop, _dropdown, _dropdownDatepicker, _dropDownTree, _event, _focusMap, _highlight, _imgPreview, _infiniteScroll, _layout, _lazyload, _loading, _magnifier, _pager, _panel, _placeholder, _popover, _popTips, _rating, _resizable, _router, _slider, _switch, _tagSelector, _template, _tooltips, _tree, _treeTable, _upload, _util, _validate, _validateTooltips, _waterfall, _window, _wordLimit){
     return window.KM={
         "ajax" : _ajax,
         "App" : _app,
@@ -11406,6 +11551,7 @@ define("KM", ["km/ajax", "km/app", "km/autoComplete", "km/cache", "km/clipZoom",
         "slider" : _slider,
         "Switch" : _switch,
         "tagSelector" : _tagSelector,
+        "Template" : _template,
         "Tooltips" : _tooltips,
         "Tree" : _tree,
         "TreeTable" : _treeTable,
---
+++ js/km/template.js
@@ -0,0 +1,144 @@
+﻿/*
+ * 模板
+ * @date:2015-09-28
+ * @author:kotenei(kotenei@qq.com)
+ */
+define('km/template', ['jquery'], function ($) {
+
+    var index = 0;
+
+    var fileName = 'tpl_' + index;
+
+    var keywords =
+        'break,case,catch,continue,debugger,default,delete,do,else,false'
+      + ',finally,for,function,if,in,instanceof,new,null,return,switch,this'
+      + ',throw,true,try,typeof,var,void,while,with';
+
+    var tags = {
+        open: '{{',
+        close: '}}'
+    };
+
+
+    var util = {
+        each: function (arr, callback) {
+            for (var i = 0, item; i < arr.length; i++) {
+                item = arr[i];
+                if (callback(i, item) == false) {
+                    break;
+                }
+            }
+        }
+    };
+
+    var method = {
+
+        escape: function (code) {
+            return "'" + code
+            // 单引号与反斜杠转义
+            .replace(/('|\\)/g, '\\$1')
+            // 换行符转义(windows + linux)
+            .replace(/\r/g, '\\r')
+            .replace(/\n/g, '\\n') + "'";
+        },
+
+        html: function (code) {
+
+
+            code = code.replace(/\s+/g, ' ').replace(/<!--[\w\W]*?-->/g, '');
+
+            if (code) {
+                code = "$out+=" + method.escape(code) + ";" + "\n";
+            }
+
+
+            return code;
+        },
+        logic: function (code) {
+
+            code = method.parser(code);
+
+            console.log(code)
+
+            return code;
+
+        },
+        parser: function (code) {
+
+            code = code.replace(/^\s/, '');
+
+            var split = code.split(' ');
+            var key = split.shift();
+            var args = split.join(' ');
+
+            switch (key) {
+                case 'each':
+
+                    var item = split[0],
+                        data = split[2];
+
+                    code = "util.each(data,function(index," + item + "){";
+
+                    break;
+                case '/each':
+
+                    code = '})';
+
+                    break;
+                default:
+                    break;
+            }
+
+            return code;
+        }
+    };
+
+
+
+    var Template = function ($el, data) {
+        ++index;
+        this.$el = $el;
+        this.data = data;
+        this.source = this.$el.html().replace(/^\s*|\s*$/g, '');
+        this.fileName = fileName;
+        this.init();
+    };
+
+    Template.prototype.init = function () {
+
+        var mainCode = "$out='';";
+
+        util.each(this.source.split(tags.open), function (index, code) {
+            var code = code.split(tags.close);
+            var $c0 = code[0];
+            var $c1 = code[1];
+
+            if (code.length == 1) {
+                mainCode += method.html($c0);
+            } else {
+                mainCode += method.logic($c0);
+
+                if ($c1) {
+                    mainCode += method.html($c1);
+                }
+            }
+
+        });
+
+        //console.log(mainCode)
+
+    };
+
+    Template.prototype.compile = function () {
+
+    };
+
+    Template.prototype.render = function () {
+        var html = '';
+
+        return html;
+    };
+
+    return Template;
+
+});
